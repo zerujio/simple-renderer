26d7c8e7edab07c655c08f2873eddbfbf9c74567
Sergio Morales <semoralesd@gmail.com>
2022-09-09 01:30:22 -0400

    moved out glsl_syntax, added stb_image to lib

--- src/glsl_definitions.cpp
+++ src/glsl_definitions.cpp
@@ -2,61 +2,63 @@
 
 namespace simple {
 
+    using namespace glutils;
+
     // Vertex attributes
 
-    const glsl::Definition vertex_position_def
+    const Definition vertex_position_def
     {
             .layout     {.location = 0},
-            .storage    = glsl::Storage::in,
-            .type       = glsl::Type::vec3_,
+            .storage    = StorageQualifier::in,
+            .type       = Type::vec3_,
             .name       = "vertex_position"
     };
 
-    const glsl::Definition vertex_normal_def
+    const Definition vertex_normal_def
     {
             .layout     {.location = 1},
-            .storage    = glsl::Storage::in,
-            .type       = glsl::Type::vec3_,
+            .storage    = StorageQualifier::in,
+            .type       = Type::vec3_,
             .name       = "vertex_normal"
     };
 
-    const glsl::Definition vertex_uv_def
+    const Definition vertex_uv_def
     {
             .layout     {.location = 2},
-            .storage    = glsl::Storage::in,
-            .type       = glsl::Type::vec2_,
+            .storage    = StorageQualifier::in,
+            .type       = Type::vec2_,
             .name       = "vertex_uv"
     };
 
     // Uniforms
 
-    const glsl::Definition model_matrix_def
+    const Definition model_matrix_def
     {
             .layout     {.location = 0},
-            .storage    = glsl::Storage::uniform,
-            .type       = glsl::Type::mat4_,
+            .storage    = StorageQualifier::uniform,
+            .type       = Type::mat4_,
             .name       = "model_matrix",
             .init       {"mat4(1.0f)"}
     };
 
-    const glsl::BlockDefinition camera_uniform_block_def
+    const BlockDefinition camera_uniform_block_def
     {
-            .layout     {.memory = glsl::Layout::Memory::std140, .binding = 0},
-            .storage    = glsl::Storage::uniform,
+            .layout     {.memory = LayoutQualifiers::Memory::std140, .binding = 0},
+            .storage    = StorageQualifier::uniform,
             .block_name = "Camera",
             .defs {
-               {.type = glsl::Type::mat4_, .name = "view_matrix"},
-               {.type = glsl::Type::mat4_, .name = "proj_matrix"}
+               {.type = Type::mat4_, .name = "view_matrix"},
+               {.type = Type::mat4_, .name = "proj_matrix"}
             }
     };
     const std::size_t view_matrix_block_index = 0;
     const std::size_t proj_matrix_block_index = 1;
 
     // Fragment outputs
-    const glsl::Definition frag_color_def
+    const Definition frag_color_def
     {
-            .storage    = glsl::Storage::out,
-            .type       = glsl::Type::vec4_,
+            .storage    = StorageQualifier::out,
+            .type       = Type::vec4_,
             .name       = "frag_color"
     };
 } // simple
--- src/glsl_definitions.hpp
+++ src/glsl_definitions.hpp
@@ -1,7 +1,7 @@
 #ifndef SIMPLERENDERER_GLSL_DEFINITIONS_HPP
 #define SIMPLERENDERER_GLSL_DEFINITIONS_HPP
 
-#include "glsl_syntax.hpp"
+#include "glutils/glsl_syntax.hpp"
 
 namespace simple {
 
@@ -9,19 +9,19 @@ namespace simple {
     constexpr auto glsl_version_c_str = "#version 430 core\n";
 
     // Vertex attribute declarations
-    extern const glsl::Definition vertex_position_def;
-    extern const glsl::Definition vertex_normal_def;
-    extern const glsl::Definition vertex_uv_def;
+    extern const glutils::Definition vertex_position_def;
+    extern const glutils::Definition vertex_normal_def;
+    extern const glutils::Definition vertex_uv_def;
 
     // Uniform declarations
 
-    extern const glsl::Definition model_matrix_def;
-    extern const glsl::BlockDefinition camera_uniform_block_def;
+    extern const glutils::Definition model_matrix_def;
+    extern const glutils::BlockDefinition camera_uniform_block_def;
     extern const std::size_t view_matrix_block_index;
     extern const std::size_t proj_matrix_block_index;
 
     // Fragment output definitions
-    extern const glsl::Definition frag_color_def;
+    extern const glutils::Definition frag_color_def;
 } // simple
 
 #endif //SIMPLERENDERER_GLSL_DEFINITIONS_HPP
