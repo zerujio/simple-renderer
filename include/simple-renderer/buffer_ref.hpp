656ec98f779b73b1a8e8762d1e51bac07362d2f4
Sergio <semorales@ug.uchile.cl>
2023-02-19 12:06:23 -0300

    BufferRef

--- include/simple-renderer/buffer_ref.hpp
+++ include/simple-renderer/buffer_ref.hpp
@@ -3,106 +3,108 @@
 
 #include "glutils/buffer.hpp"
 
-#include <type_traits>
 #include <stdexcept>
 
 namespace simple {
 
-template<bool Read, bool Write>
-class BufferRef final
+class BufferRef
 {
 public:
-    BufferRef() = default;
-
     BufferRef(GL::BufferHandle buffer, std::uintptr_t offset, std::size_t size)
-            : m_buffer(buffer), m_offset(offset), m_size(offset)
+            : m_buffer(buffer), m_offset(static_cast<GLintptr>(offset)), m_size(static_cast<GLsizeiptr>(size))
     {}
 
-    template<bool NewRead, bool NewWrite>
-    constexpr operator const BufferRef<NewRead, NewWrite> &() const
-    {
-        static_assert((!NewRead || Read) && (!NewWrite || Write),
-                      "can't convert to buffer reference type with greater access permissions");
-
-        return reinterpret_cast<const BufferRef<NewRead, NewWrite> &>(*this);
-    }
-
-    template<bool NewRead, bool NewWrite>
-    constexpr operator BufferRef<NewRead, NewWrite> &()
-    { return const_cast<BufferRef<NewRead, NewWrite> &>(this->operator const BufferRef<NewWrite, NewRead> &()); }
-
-    [[nodiscard]] std::uintptr_t getOffset() const
+    [[nodiscard]] constexpr std::uintptr_t getOffset() const
     { return m_offset; }
 
-    [[nodiscard]] std::size_t getSize() const
+    [[nodiscard]] constexpr std::size_t getSize() const
     { return m_size; }
 
-    std::enable_if_t<Write, void> write(const void *data) const
-    { m_buffer.write(m_offset, m_size, data); }
-
-    std::enable_if_t<Read, void> read(void *data) const
-    { m_buffer.read(m_offset, m_size, data); }
-
-    template<bool FromWrite, bool ToRead>
-    friend void copyBufferData(const BufferRef<true, FromWrite> &from, const BufferRef<ToRead, true> &to);
-
-    template<bool OtherRead>
-    std::enable_if_t<Write, void> copyTo(const BufferRef<OtherRead, true> &other) const
-    { copyBufferData(*this, other); }
-
-    template<bool OtherWrite>
-    std::enable_if_t<Read, void> copyFrom(const BufferRef<true, OtherWrite> &other) const
-    { copyBufferData(other, *this); }
-
-private:
+protected:
     GL::BufferHandle m_buffer{};
     GLintptr m_offset{0};
     GLsizeiptr m_size{0};
+
+    void m_write(const void *data) const
+    { m_buffer.write(m_offset, m_size, data); }
+
+    void m_read(void *data) const
+    { m_buffer.read(m_offset, m_size, data); }
+
+    static void s_copy(const BufferRef &from, const BufferRef &to)
+    {
+        if (from.m_size != to.m_size)
+            throw std::logic_error("attempt to copy data between buffer ranges of different size");
+
+        GL::BufferHandle::copy(from.m_buffer, to.m_buffer, from.m_offset, to.m_offset, from.m_size);
+    }
+
+    void m_copyTo(const BufferRef &other) const
+    { s_copy(*this, other); }
+
+    void m_copyFrom(const BufferRef &other) const
+    { s_copy(other, *this); }
 };
 
-template<bool FromWrite, bool ToRead>
-void copyBufferData(const BufferRef<true, FromWrite> &from, const BufferRef<ToRead, true> &to)
+class RBufferRef;
+
+class WBufferRef;
+
+struct RBufferRef final : BufferRef
 {
-    if (from.m_size != to.m_size)
-        throw std::logic_error("copy between buffer sections of different size");
+    using BufferRef::BufferRef;
 
-    GL::BufferHandle::copy(from.m_buffer, to.m_buffer, from.m_offset, to.m_offset, from.m_size);
-}
+    void read(void *data) const
+    { m_read(data); }
 
-using RBufferRef = BufferRef<true, false>;
-using WBufferRef = BufferRef<false, true>;
-using RWBufferRef = BufferRef<true, true>;
-
-/// Polymorphic wrapper for buffer write operations.
-struct BufferWriteOperation
-{
-    virtual void operator()(const RBufferRef &readable_buffer) = 0;
+    inline void copyTo(const WBufferRef &other);
 };
 
-/// Polymorphic functor wrapper for BufferRef::read()
-class WriteToBuffer : public BufferWriteOperation
+struct WBufferRef final : BufferRef
 {
 public:
-    WriteToBuffer(size_t size, const void *data) : m_size(size), m_data(data)
-    {}
+    using BufferRef::BufferRef;
 
-    void operator()(const RBufferRef &readable_buffer) override;
+    void write(const void *data) const
+    { m_write(data); }
 
-private:
-    std::size_t m_size;
-    const void *m_data;
+    inline void copyFrom(const RBufferRef &other);
 };
 
-/// Polymorphic functor wrapper for BufferRef::copyTo()
-class CopyToBuffer : public BufferWriteOperation
+void RBufferRef::copyTo(const WBufferRef &other)
+{ m_copyTo(other); }
+
+void WBufferRef::copyFrom(const RBufferRef &other)
+{ m_copyFrom(other); }
+
+struct RWBufferRef final : BufferRef
 {
-public:
+    using BufferRef::BufferRef;
 
-private:
-    GL::Buf
+    void write(const void *data) const
+    { m_write(data); }
+
+    void read(void *data) const
+    { m_read(data); }
+
+    inline void copyTo(const WBufferRef &other)
+    { m_copyTo(other); }
+
+    inline void copyFrom(const RBufferRef &other)
+    { m_copyFrom(other); }
+
+    [[nodiscard]] WBufferRef toWriteOnlyRef() const
+    { return {m_buffer, getOffset(), getSize()}; }
+
+    [[nodiscard]] RBufferRef toReadOnlyRef() const
+    { return {m_buffer, getOffset(), getSize()}; }
+
+    [[nodiscard]] operator WBufferRef() const
+    { return toWriteOnlyRef(); }
+
+    [[nodiscard]] operator RBufferRef() const
+    { return toReadOnlyRef(); }
 };
-
-/// Polymorphic wrapper for buffer read operations.
 
 } // simple
 
